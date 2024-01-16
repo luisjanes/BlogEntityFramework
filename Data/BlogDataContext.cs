ca8bf87cd97018cb5ee4b43ae150208df8c4e9aa
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2024-01-16 17:43:11 -0300

    Adicionar arquivos de projeto.

---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,27 @@
+﻿using BlogEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogEntityFramework.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        //public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        //public DbSet<UserRole> UserRoles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=Blog;User ID=root;Password=123456", ServerVersion.AutoDetect("Server=localhost;Database=Blog;User ID=root;Password=123456"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using BlogEntityFramework.Data;
+using BlogEntityFramework.Models;
+
+namespace BlogEntityFramework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(var context = new BlogDataContext())
+            {
+                var tag = new Tag { Id = 0, Name = "teste", Slug = "test" };
+                context.Tags.Add(tag);
+                context.SaveChanges();
+            }
+        }
+    }
+}
